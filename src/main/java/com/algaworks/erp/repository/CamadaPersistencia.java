bb0f791bcd48f5a6b0907cc907d0786f0ba78b95
Leandro Menezes <lmsilv@gmail.com>
2024-12-04 11:40:51 -0300

    Configurando CDI no projeto

--- src/main/java/com/algaworks/erp/repository/CamadaPersistencia.java
+++ src/main/java/com/algaworks/erp/repository/CamadaPersistencia.java
@@ -14,7 +14,7 @@ import com.algaworks.erp.model.TipoEmpresa;
 public class CamadaPersistencia {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AlgaWorksPU");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AlgaworksPU");
 
         EntityManager em = emf.createEntityManager();
 
